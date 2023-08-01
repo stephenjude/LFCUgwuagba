32e37fbd7e5b75ba5364edfb317fd1b4258b41b1
stephenjude <stephenjudesuccess@gmail.com>
2023-08-01 12:13:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,277 @@
+ <!-- comments -->
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>welcome</title>
+ <!-- calling css libries -->
+ <link href="css/style.css" rel="stylesheet" />
+ <link href="JScript/jquery-ui.css" rel="stylesheet" />
+ <link href="JScript/jquery-ui.theme.css" rel="stylesheet" />
+ <link rel="stylesheet"  href="engine1/style.css" />
+
+  <!-- end of calling css libries -->
+ 
+ <!-- calling Jscript libries -->
+  <script src="JScript/jquery.js" ></script>
+  <script src="JScript/jquery-ui.js" ></script>
+   <!-- end of calling jscript libries -->
+   
+  <script>
+$(function() {  
+
+//Read More Button
+ $( "button" ).button({
+ }); 
+   
+	
+//Nes Update Side bar		 
+$(function() {  
+	    $( "#newsUpdate" ).accordion({
+				
+			}); 
+			 
+	  });  
+ 
+});
+
+</script>
+</head>
+<body>
+
+<!-- CREATING BODY RAPPER -->
+<div id="bodyRapper">
+
+<!-- CREATING HEADER RAPPER -->
+	<header id="header">
+    
+    <!-- CREATING HEADING -->
+    <div id="head">
+    
+    <div id="logoText">
+    <h1>LIVING FAITH CHURCH UGWUAGBA</h1>
+    <h4>Home Of Signs And Wonders</h4>
+    </div>
+    
+    <div id="logo">
+    <img src="images/LFC_log.png" id="logoImage" style=" border-radius:20px;"/>	
+    </div>
+    
+        </div>
+     <!-- END OF HEADING -->
+     
+         <!-- CREATING NAVIGATION -->
+    <div id="navigation">
+    <ul>
+	 <a href=""><li id="">Home</li></a>
+    <a href=""><li id="">Mandate</li></a>
+    <a href=""><li id="">News</li></a>
+    <a href=""><li id="">Gallary</li></a>
+    <a href=""><li id="">Store</li></a>
+    <a href=""><li id="">About</li></a>
+    <a href=""><li id="">Contact</li></a>
+    </ul>
+    </div>
+     <!-- END OF NAVIGATION -->
+    
+    </header>
+<!--END OF HEADER RAPPER -->
+
+<!--CREATING SLIDER WRAPPER -->
+<div id="sliderWrapper">
+	
+    	<!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+    
+<li><img src="data1/images/p4.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+<li><img src="data1/images/p4.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+
+<li><img src="data1/images/p4.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+<li><img src="data1/images/p4.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+
+<li><img src="data1/images/p2.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+<li><img src="data1/images/p2.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+
+<li><img src="data1/images/p3.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+<li><img src="data1/images/p3.png" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="Chrysanthemum"></a>
+</div></div><span class="wsl"><a href="">jquery carousel</a></span>
+	<div class="ws_shadow"></div>
+	</div>
+    
+</div>
+<!--END OF SLIDER WRAPPER -->
+
+
+<!--CREATING MAIN CONTAINER -->
+<div id="container" >
+
+<!-- CENTER CONTAINER --> 
+<div id="NewsLetterContainer" class="container">
+	<h2>Welcome Speech From The Resident Pastor</h2>
+<div id="NewsLetter">
+<P>
+ Welcome to 2015
+With a gladsome heart, I specially welcome you to this brand new year 2015 - A year in which God will cause your eyes to see things which no man has ever seen nor ear heard. 1 Cor.2:9 With a gladsome heart, I specially welcome you to this brand new year 2015 - A year in which God will cause your eyes to see things which no man has ever seen nor ear heard. 1 Cor.2:9
+</P>
+
+<p>
+The scripture makes us to understand that every true prophetic word is a sworn verdict, that is, the contents remain binding on God to fulfill when our part of the deal is settled. “The LORD of hosts hath sworn, saying, Surely as I have though, so shall it come to pass; and as I have purposed, so shall it stand” – Is.14:24	
+With a gladsome heart, I specially welcome you to this brand new year 2015 - A year in which God will cause your eyes to see things which no man has ever seen nor ear heard. 1 Cor.2:9
+</p>
+
+<p>
+The scripture makes us to understand that every true prophetic word is a sworn verdict, that is, the contents remain binding on God to fulfill when our part of the deal is settled. “The LORD of hosts hath sworn, saying, Surely as I have though, so shall it come to pass; and as I have purposed, so shall it stand” – Is.14:24	
+</p>
+<H4 id"letterFooter">Pst. Wellington E. Aligbe - Resident Pastor</H4>
+
+</div>
+
+</div>
+<!-- END OF CENTER CONTAINER --> 
+
+<!-- LEFT CONTAINER -->
+<div  id="AdvertContainerLeft">
+
+<h2 class="SideBarHeader">Latest News Updates</h2>
+<div id="newsUpdate">
+	<h5>Latest News That you can read online on our website</h5>
+    <div> "The LORD of hosts hath sworn, saying, Surely as I have though, so shall it come to pass; and as I have purposed,   <button id="readmore">Read More</button>
+    </div>
+    
+    	<h5>Latest News That you can read online on our website</h5>
+    <div> "The LORD of hosts hath sworn, saying, Surely as I have though, so shall it come to pass; and as I have purposed,   <button id="readmore">Read More</button>
+    </div>
+    
+    	<h5>Latest News That you can read online on our website</h5>
+    <div> "The LORD of hosts hath sworn, saying, Surely as I have though, so shall it 					come to pass; and as I have purposed,	<button id="readmore">Read More</button>
+    </div>
+    
+    	<h5>Latest News That you can read online on our website</h5>
+    <div> "The LORD of hosts hath sworn, saying, Surely as I have though, so shall it come to pass; and as I have purposed,   <button id="readmore">Read More</button>
+    </div>	
+    
+        
+</div>
+
+<div id="ButtonContainer">
+	<button id="MoreNewsButton">More News</button>
+</div>
+
+</div>
+<!-- END OF LEFT CONTAINER -->
+
+<!-- RIGHT CONTAINER -->
+<div id="AdvertContainerRight" class="container">
+
+<div id="Advert1" class="container">
+	<img src="images/media.jpg" id="advertImg" />
+		<button id="advertBtn1">
+			Visit our Media Centre 
+		</button>
+</div>
+
+<div id="Advert2" class="container">
+ 	<img src="images/store.jpg" id="advertImg" />
+ 		<button id="advertBtn2">
+        	Visit our bookstore
+        </button>	
+</div>
+
+</div>
+<!-- END OF RIGHT CONTAINER -->
+
+	
+</div>
+<!--END OF MAIN CONTAINER -->
+
+
+<!--CREATING FOOTER-->
+<div id="footer">
+
+<div id="footerHeader">
+
+<div id="firstFooter" class="footer">
+	<ul id="footerMenu">
+	<li id="home">Home</li>
+    <li id="mandate">Mandate</li>
+    <li id="news">News</li>
+    <li id="gallary">Gallary</li>
+    <li id="store">Store</li>
+    <li id="about">About</li>
+    <li id="cantact">Contact</li> 
+    </ul>
+</div>
+
+<div id="secondFooter" class="footer">
+	<ul id="footerMenu">
+    <li id="Testimonies">Online Testimonies</li>
+    <li id="propheticFocus">Prophetic Focus</li>
+    <li id="BooksOfTheMonth">Books of the Month</li>
+  	<li id="serviceUnits">Service Units</li>
+		 <li id="shareYourTestimonies">Share Your Testimonies</li>
+         <li id="makeYourPrayerRequest">Make Your Prayer Request</li>	
+         <li id="visitDominionBookstore">Visit Dominion Bookstore</li>
+    </ul>
+</div>
+
+<div id="thirdFooter" class="footer">
+	<ul id="footerMenu">
+         <li id="">David Oyedepo</li>	
+         <li id="">David Abioye</li>	
+         <li id="">Faith Oyedepo</li>	
+         <li id="">Covenant University</li>
+         <li id="">Landmark University</li>
+         <li id="">Winners Chapel USA</li>
+         <li id="">Winners Chapel SA</li>
+    </ul>
+</div>
+
+<div id="fourthFooter" class="footer">
+		<div id="footerMenu">
+        <h2>Location :</h2>
+        <h4>Address:</h4>
+        <h5>#4 Azike Drive Awada Obosi</h5>
+		<h5>Off Onitsha-Owerri Road Express-Way</h5>
+        <h5>Onitsha Anambra State</h5>
+        <h4>Tel :</h4>
+        <h5>123-456-789</h5>
+   		</div>
+</div>
+
+<div id="fifthFooter" class="footer">
+	<ul id="footerMenu">
+		   			<li id="facebook"><img src="images/facebook.png" id=""/><p>Like us on facebook</p></li>
+            <li id="twitter"><img src="images/twitter.png" id=""/><p>Follow us on twitter</p></li>
+            <li id="youtube"><img src="images/youtube.png" id=""/><p>Watch  us on youtube</p></li>
+            <li id=""><p></p></li>
+            
+    </ul>
+</div>
+
+</div>
+
+<div id="mainFooter">
+<ul>
+<li><h4>copyright@lfcUguagba.org</h4></li>
+<li><h5>Designed by Stephen Jude</h5></li>
+</ul>
+</div>
+
+</div>
+<!--END OF FOOTER-->
+
+</div>		
+ <!-- END OF BODY RAPPER -->
+ 
+   <script type="text/javascript" src="engine1/wowslider.js"></script>
+  <script type="text/javascript" src="engine1/script.js"></script>
+	
+
+   <!-- end of calling jscript libries -->
+ 
+</body>
+</html>
